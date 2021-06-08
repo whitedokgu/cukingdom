c0d231f899d684f2a01b3d452b93a554a613eb96
whitedokgu <npro107@gmail.com>
2021-06-08 14:31:15 +0900

    Update

--- kingdom/js/topbtn.js
+++ kingdom/js/topbtn.js
@@ -7,3 +7,8 @@ window.addEventListener("scroll", () => {
     toTop.classList.remove("active");
   }
 })
+
+$( '.to-top' ).click( function() {
+	$( 'html, body' ).animate( { scrollTop : 0 }, 400 );
+	return false;
+} );
